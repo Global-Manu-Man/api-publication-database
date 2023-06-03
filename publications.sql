f66a29f4487996320e56abc29fed75d107847560
Emmanuel Sandoval <84020431+Global-Manu-Man@users.noreply.github.com>
2023-06-03 10:40:49 -0600

    Update publications.sql
    update creation databases.

--- publications.sql
+++ publications.sql
@@ -22,7 +22,12 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
+CREATE DATABASE locallity;
+use locallity;
 
+CREATE USER 'mysqluser'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+GRANT ALL PRIVILEGES ON locallity.* TO 'mysqluser'@'%';
+FLUSH PRIVILEGES;
 --
 -- Table structure for table `business`
 --
